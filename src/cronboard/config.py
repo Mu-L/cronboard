da54761793741cf05ac20522ccfe0d156384ba4e
Antonio Rodriguez <102554647+antoniorodr@users.noreply.github.com>
2026-07-20 19:52:22 +0200

    style(config): add typing

--- src/cronboard/config.py
+++ src/cronboard/config.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-CONFIG_REL_PATH = ".config/cronboard"
-WRAPPER_DIST = "cron-wrapper.sh"
-LOG_REL_PATH = f"{CONFIG_REL_PATH}/logs"
-CONFIG_DIR = Path.home() / CONFIG_REL_PATH
-CONFIG_FILE = CONFIG_DIR / "servers.toml"
-KEY_FILE = CONFIG_DIR / "secret.key"
-LOG_DIR = CONFIG_DIR / "logs"
-WRAPPER_SOURCE = Path(__file__).parent / "logging" / "cron-wrapper.sh"
+CONFIG_REL_PATH: str = ".config/cronboard"
+WRAPPER_DIST: str = "cron-wrapper.sh"
+LOG_REL_PATH: str = f"{CONFIG_REL_PATH}/logs"
+CONFIG_DIR: Path = Path.home() / CONFIG_REL_PATH
+CONFIG_FILE: Path = CONFIG_DIR / "servers.toml"
+KEY_FILE: Path = CONFIG_DIR / "secret.key"
+LOG_DIR: Path = CONFIG_DIR / "logs"
+WRAPPER_SOURCE: Path = Path(__file__).parent / "logging" / "cron-wrapper.sh"
