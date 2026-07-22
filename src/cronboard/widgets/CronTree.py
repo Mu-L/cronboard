0a392770701ef466eddc9d5227ec829312188b79
Antonio Rodriguez <102554647+antoniorodr@users.noreply.github.com>
2026-07-22 11:35:35 +0200

    docs(CronTree): add docstrings

--- src/cronboard/widgets/CronTree.py
+++ src/cronboard/widgets/CronTree.py
@@ -3,6 +3,8 @@ from textual.widgets import Tree
 
 
 class CronTree(Tree):
+    """Textual Tree widget with Vim-like keyboard navigation."""
+
     BINDINGS = [Binding("j", "cursor_down", "Down"), Binding("k", "cursor_up", "Up")]
 
     def __init__(self, *args, **kwargs):
