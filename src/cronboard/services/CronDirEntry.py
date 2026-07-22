3d855c008e81ce245b8f8db9f30f8b418692bf82
Antonio Rodriguez <102554647+antoniorodr@users.noreply.github.com>
2026-07-22 11:07:47 +0200

    docs(CronDirEntry): add docstrings

--- src/cronboard/services/CronDirEntry.py
+++ src/cronboard/services/CronDirEntry.py
@@ -1,8 +1,22 @@
 class CronDirEntry:
+    """Represents a directory entry, mirroring the os.DirEntry object.
+
+    Attributes:
+        name: The name of the directory entry.
+        path: The path of the directory entry.
+        is_dir: Whether the directory entry is a directory.
+    """
+
     def __init__(self, name: str, path: str, is_dir: bool):
         self.name: str = name
         self.path: str = path
         self._is_dir: bool = is_dir
 
     def is_dir(self):
+        """Returns whether the directory entry is a directory.
+
+        Returns:
+            True if the directory entry is a directory, else False.
+        """
+
         return self._is_dir
